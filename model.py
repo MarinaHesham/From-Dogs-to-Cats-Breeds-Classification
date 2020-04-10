a65b0c917b0f70028c9510d7a1c46298ec083c1d
marinahesham <marinahesham21@gmail.com>
2020-04-09 20:19:56 -0400

    initial files commit

---
+++ model.py
@@ -0,0 +1,2 @@
+# Create a model to classify dogs breeds
+# Retrain the classification head and fine tune the model to classify cats breeds
---
+++ preprocessing.py
@@ -0,0 +1,1 @@
+# create a function get_data that returns train_images, train_labels, test_images, test_labels
