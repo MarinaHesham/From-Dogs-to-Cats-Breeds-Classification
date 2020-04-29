3a1362ad315464dcb2cf0e0c529f1402093281d8
marinahesham <marinahesham21@gmail.com>
2020-04-29 15:39:42 -0400

    rm extrafile model.py

--- model.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-# Create a model to classify dogs breeds
-# Retrain the classification head and fine tune the model to classify cats breeds
-
-def run_model(train_images, train_labels, test_images, test_labels):
-    return
